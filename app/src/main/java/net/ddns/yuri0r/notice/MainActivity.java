12c31334e788275d71a839eaf73e6fa4bd184e39
yuri0r <macktrinz@yahoo.de>
2017-06-26 23:25:01 +0200

    first preparations for adapter

--- app/src/main/java/net/ddns/yuri0r/notice/MainActivity.java
+++ app/src/main/java/net/ddns/yuri0r/notice/MainActivity.java
@@ -2,12 +2,18 @@ package net.ddns.yuri0r.notice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    NoticeDbHelper dbHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dbHelper = new NoticeDbHelper(this);
+        ListView listView = (ListView) findViewById(R.id.listView);
     }
+
 }
