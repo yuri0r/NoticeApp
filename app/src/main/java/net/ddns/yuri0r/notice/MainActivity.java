42ad93323d5d216f7b64fe57d3622679ef09dab8
yuri0r <macktrinz@yahoo.de>
2017-06-27 02:14:43 +0200

    made main fill the liestview

--- app/src/main/java/net/ddns/yuri0r/notice/MainActivity.java
+++ app/src/main/java/net/ddns/yuri0r/notice/MainActivity.java
@@ -1,8 +1,10 @@
 package net.ddns.yuri0r.notice;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         dbHelper = new NoticeDbHelper(this);
+        dbHelper.addTask("test","it works");
+/*-----------------------UI Elements--------------------------------------------------------*/
         ListView listView = (ListView) findViewById(R.id.listView);
+
+
+/*--------------------Other Variables----------------*/
+        Cursor todoCursor = dbHelper.getTasks();
+        TodoCursorAdapter todoAdapter = new TodoCursorAdapter(this, todoCursor);
+        listView.setAdapter(todoAdapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        dbHelper.close();
+        super.onDestroy();
     }
 
 }
